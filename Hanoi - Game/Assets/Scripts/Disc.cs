bc1fb3e91543ab2ed967227d33e3e75adeb26ab6
steven <savandenhoven@gmail.com>
2019-12-13 20:03:06 +0100

    Unity Hanoi Game

---
+++ Hanoi - Game/Assets/Scripts/Disc.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disc : MonoBehaviour
+{
+    private enum Status
+    {
+        Waiting_For_First_Click = 0,
+        Waiting_For_Second_Click,
+        Move_To_Middle_Point,
+        Move_To_End_Position,
+    }
+
+    [SerializeField] private Status m_Status;
+    private Vector3 m_StartLocation;
+    private Vector3 m_EndLocation;
+    private Vector3 m_Position;
+    private float m_TurnTimer;
+    [SerializeField, Range(0.1f, 10f)] private float m_Speed;
+    [SerializeField] public Rigidbody m_Body;
+
+    public Disc()
+    {
+        m_Speed = 5;
+    }
+
+    private void Start()
+    {
+        m_Body.useGravity = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MoveToLocation();
+    }
+
+    public void SetPosition(Vector3 endPos)
+    {
+        m_EndLocation = endPos;
+        m_Status = Status.Move_To_Middle_Point;
+    }
+
+    public void MoveToLocation()
+    {
+        if (m_Status == Status.Move_To_Middle_Point)
+        {
+            m_TurnTimer += Time.deltaTime;
+            GameObject middle = GameObject.Find("MidLocation");
+            m_StartLocation = gameObject.transform.position;
+            float t = 0;
+            t += m_TurnTimer * m_Speed;
+            transform.position = Vector3.Lerp(m_StartLocation, middle.transform.position, t);
+            print(t.ToString());
+            if (t >= 1)
+            {
+                ResetTimers();
+                m_Status = Status.Waiting_For_First_Click;
+                SetCurrentLocation();
+                m_Body.useGravity = true;
+            }
+        }
+        if (m_Status == Status.Move_To_End_Position)
+        {
+            m_TurnTimer += Time.deltaTime;
+            m_StartLocation = gameObject.transform.position;
+            float t = 0;
+            t += m_TurnTimer * m_Speed;
+            transform.position = Vector3.Lerp(m_StartLocation, m_EndLocation, t);
+            if (t >= 1)
+            {
+                ResetTimers();
+                m_Status = Status.Waiting_For_First_Click;
+            }
+        }
+    }
+
+    private void ResetTimers()
+    {
+        m_TurnTimer = 0;
+    }
+
+    private void SetCurrentLocation()
+    {
+        gameObject.transform.position = transform.position;
+    }
+}
---
+++ Hanoi - Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Disc[] m_Dics = new Disc[5];
+    [SerializeField] private GameObject m_Middle;
+
+    [SerializeField] private Stack<GameObject> m_Pole1 = new Stack<GameObject>();
+    [SerializeField] private Stack<GameObject> m_Pole2 = new Stack<GameObject>();
+    [SerializeField] private Stack<GameObject> m_Pole3 = new Stack<GameObject>();
+
+    private Vector3 m_Pole1X;
+    private float m_Pole2X;
+    private float m_Pole3X;
+
+    private void Awake()
+    {
+        Pole1Load();
+    }
+
+    private void Start()
+    {
+        m_Dics[0].SetPosition(m_Middle.transform.position);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void Pole1Load()
+    {
+        GameObject pole1 = GameObject.Find("Pole0");
+        GameObject pole2 = GameObject.Find("Pole1");
+        GameObject pole3 = GameObject.Find("Pole2");
+        m_Pole1X = pole1.transform.position;
+        m_Pole2X = pole2.transform.position.x;
+        m_Pole3X = pole3.transform.position.x;
+        for (int i = m_Dics.Length - 1; i > -1; i--)
+        {
+            Vector3 pos = m_Dics[i].gameObject.transform.position;
+            m_Dics[i].gameObject.transform.position = new Vector3(m_Pole1X.x, pos.y, pos.z);
+            m_Pole1.Push(m_Dics[i].gameObject);
+        }
+    }
+}
