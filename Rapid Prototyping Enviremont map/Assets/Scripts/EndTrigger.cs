9aff648ef39a8ed1dad516f67867a12b230b82dd
steven <savandenhoven@gmail.com>
2020-01-28 16:56:04 +0100

    update

---
+++ Rapid Prototyping Enviremont map/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    [SerializeField] private GameManager m_Manager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_Manager.EndGame();
+        }
+    }
+}
--- Rapid Prototyping Enviremont map/Assets/Scripts/GameManager.cs
+++ Rapid Prototyping Enviremont map/Assets/Scripts/GameManager.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Canvas m_Canvas;
+
     [SerializeField] private Vector3 m_CheckPointPositon;
     [SerializeField] private GameObject m_Player = null;
     private Vector3 m_Offset = new Vector3(0, 4, 0);
@@ -29,6 +32,25 @@ public class GameManager : MonoBehaviour
         //m_Player.GetComponent<FirstPersonController>().enabled = false;
         m_Player.transform.position = m_CheckPointPositon;
         //Invoke("TurnOnFirstPersonController", 0.1f);
+    }
+
+    public void EndGame()
+    {
+        Animator animator = m_Canvas.GetComponent<Animator>();
+        animator.Play("CanvasAnimation");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        m_Player.GetComponent<FirstPersonController>().enabled = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 
     public GameObject GetPlayerObject()
