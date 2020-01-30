4187e0dc7b4f024b6117b187ea385a43ff67d6f3
Stevenchiefo <savandenhoven29@gmail.com>
2020-01-30 08:14:39 +0100

    UP

--- First person shooter/Assets/Scripts/Enemy.cs
+++ First person shooter/Assets/Scripts/Enemy.cs
@@ -6,6 +6,10 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private float m_Health = 100;
     private GameObject m_Player;
+    private RaycastHit m_RaycastHit;
+    [SerializeField] private int m_Distance;
+    [SerializeField] private LayerMask m_LayerMask;
+    private bool m_PlayedDetected = false;
 
     private void Start()
     {
@@ -15,6 +19,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        DetectPlayer();
         LookAtPlayer();
         Death();
     }
@@ -37,6 +42,25 @@ public class Enemy : MonoBehaviour
     public bool IsDeath()
     {
         return m_Health <= 0;
+    }
+
+    private void DetectPlayer()
+    {
+        Vector3 dir = m_Player.transform.position - transform.position;
+        Physics.Raycast(transform.position, dir, out m_RaycastHit);
+
+        if (m_RaycastHit.collider != null)
+        {
+            if (m_RaycastHit.collider.gameObject.CompareTag("Player"))
+            {
+                m_PlayedDetected = true;
+            }
+            else
+            {
+                m_PlayedDetected = false;
+            }
+        }
+        Debug.DrawRay(transform.position, dir);
     }
 
     private void LookAtPlayer()
