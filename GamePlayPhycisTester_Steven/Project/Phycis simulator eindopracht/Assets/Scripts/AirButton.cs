1d8b637d2c7e7e0f0bdf7ee8c4bd611309c478ab
steven <savandenhoven@gmail.com>
2020-03-28 18:13:39 +0100

    up

---
+++ GamePlayPhycisTester_Steven/Project/Phycis simulator eindopracht/Assets/Scripts/AirButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirButton : MonoBehaviour
+{
+    [SerializeField] private AirCannon m_AirCannon;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_AirCannon.Shoot();
+    }
+}
---
+++ GamePlayPhycisTester_Steven/Project/Phycis simulator eindopracht/Assets/Scripts/Canon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+    [SerializeField] private GameObject m_CanonBall;
+    [SerializeField] private Transform m_Shootingpoint;
+    [SerializeField] private float m_Force;
+    [SerializeField] private float m_Speed;
+
+    // Update is called once per framea
+    private void Start()
+    {
+        Shoot();
+    }
+
+    private void ShootChecker()
+    {
+        if (m_CanonBall != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector3 dir = m_Shootingpoint.position - transform.position;
+        dir = dir.normalized;
+        Vector3 addyforce = new Vector3(0f, m_Force, 0f);
+        Rigidbody rigidbody = m_CanonBall.GetComponent<Rigidbody>();
+        float mass = rigidbody.mass;
+        rigidbody.AddForce((dir * m_Speed + addyforce) * mass, ForceMode.Impulse);
+        m_CanonBall = null;
+    }
+}
