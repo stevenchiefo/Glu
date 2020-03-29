1f8a74ad53ae0944ac83262a38f79b516a0b3071
steven <savandenhoven@gmail.com>
2020-03-29 14:25:36 +0200

    up

---
+++ visual studio oefenen/Discordbot/Discordbotlearning/Discordbotlearning/Bot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace Discordbotlearning
+{
+    internal class Bot
+    {
+        public async Task RunAsync()
+        {
+        }
+    }
+}
