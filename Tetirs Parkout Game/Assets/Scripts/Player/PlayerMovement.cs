a4a198d157ccf75e82213f103e29801ae82a9b85
steven vd hoven <46068940+stevenchiefo@users.noreply.github.com>
2020-10-01 11:02:32 +0200

    start of crouch

--- Tetirs Parkout Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Tetirs Parkout Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,31 +5,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody m_Player;
-    public float m_speed;
-    public float TurningSpeed;
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
-    [SerializeField] private float m_LookSpeed;
-    private Camera m_Camera;
-    private Vector2 m_ScreenCenter;
-    private Vector2 m_MousePosition;
-    private float m_LookX = 0.0f;
-    private float m_LookY = 0.0f;
-    private int m_CanJump = 2;
-    public bool IsRunning { private set; get; }
+    
 
+    private Camera m_Camera;
+    private Vector2 m_MousePosition;
+    private int m_CanJump = 2;
     private Vector3 m_MoveDirection;
 
+
+
+    [SerializeField] private float m_speed;
+    [SerializeField] private float TurningSpeed;
+    [SerializeField] private float m_LookSpeed;
     [SerializeField] private float m_MaxLookUp;
     [SerializeField] private float m_MaxLookDown;
     [SerializeField] private float m_JumpSpeed;
+    public bool IsRunning { private set; get; }
+    public bool IsCrouching { private set; get; }
 
     private void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        m_ScreenCenter = new Vector2(Screen.width, Screen.height);
+        
         m_Camera = Camera.main;
         m_Camera.transform.rotation = transform.rotation;
     }
@@ -48,6 +46,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Look()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Vector2 mouseMovement = m_MousePosition;
         mouseMovement = mouseMovement * m_LookSpeed;
@@ -89,5 +88,18 @@ public class PlayerMovement : MonoBehaviour
         m_MoveDirection = movement;
 
         IsRunning = _ReadedValue != Vector2.zero;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void Crouch(InputAction.CallbackContext context)
+    {
+        
+        IsCrouching = context.performed;
+        Debug.Log(IsCrouching);
+        
     }
 }
