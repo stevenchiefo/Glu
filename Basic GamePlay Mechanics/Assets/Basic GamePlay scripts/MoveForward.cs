e5f79e7d58dd1e499c3ca83a06531f1de0e8fb88
stevenchiefo <46068940+stevenchiefo@users.noreply.github.com>
2020-12-17 15:26:08 +0100

    .

---
+++ Basic GamePlay Mechanics/Assets/Basic GamePlay scripts/MoveForward.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    [SerializeField] private float m_Speed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
+    }
+}
---
+++ Basic GamePlay Mechanics/Assets/Basic GamePlay scripts/PlayerFarm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFarm : MonoBehaviour
+{
+    [SerializeField] private float m_Speed;
+
+    private float m_Xdir;
+    private Rigidbody m_Rb;
+
+    private void Start()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rb.MovePosition(transform.position + (Vector3.left * m_Xdir) * m_Speed * Time.deltaTime);
+    }
+
+    private void CheckInput()
+    {
+        m_Xdir = Input.GetAxis("Horizontal");
+    }
+}
